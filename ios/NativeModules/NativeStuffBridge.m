47626d3cd1c57550ef3a61f6820ca19b8bc1d4bd
AryanRogye <aryan.rogye@gmail.com>
2025-04-21 16:27:59 -0500

    removed the file content in NativeModules, make sure its empty when adding

--- ios/NativeModules/NativeStuffBridge.m
+++
@@ -1,5 +0,0 @@
-#import <React/RCTBridgeModule.h>
-
-@interface RCT_EXTERN_MODULE(NativeStuff, NSObject)
-RCT_EXTERN_METHOD(helloWorld:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
-@end
